3c95d72fc0cd2c15f31290aa49e33c4ba356c33d
André Almeida <aalalmeida13@gmail.com>
2022-11-27 21:46:56 +0000

    authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number')->nullable();
+            $table->enum('gender', ['M', 'F', 'O'])->nullable();
+            $table->date('birth_date')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
